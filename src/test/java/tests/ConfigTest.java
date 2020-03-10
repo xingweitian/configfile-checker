439d478406e8576fd381228f82d57bed62a7fe9e
xingweitian <13183370+xingweitian@users.noreply.github.com>
2020-03-10 15:07:40 -0400

    Update build.gradle and reformat the whole project.

--- src/test/java/tests/ConfigTest.java
+++ src/test/java/tests/ConfigTest.java
@@ -7,20 +7,13 @@ import org.junit.runners.Parameterized.Parameters;
 /**
  * Test runner for tests of the Config Checker Checker.
  *
- * <p>Tests appear as Java files in the {@code tests/config} folder. To add a new test case,
- * create a Java file in that directory. The file contains "// ::" comments to indicate expected
- * errors and warnings; see
- * https://github.com/typetools/checker-framework/blob/master/checker/tests/README .
+ * <p>Tests appear as Java files in the {@code tests/config} folder. To add a new test case, create
+ * a Java file in that directory. The file contains "// ::" comments to indicate expected errors and
+ * warnings; see https://github.com/typetools/checker-framework/blob/master/checker/tests/README .
  */
 public class ConfigTest extends CheckerFrameworkPerDirectoryTest {
     public ConfigTest(List<File> testFiles) {
-        super(
-                testFiles,
-                ConfigChecker.class,
-                "config",
-                "-Anomsgtext",
-                "-Astubs=stubs/",
-                "-nowarn");
+        super(testFiles, ConfigChecker.class, "config", "-Anomsgtext", "-Astubs=stubs/", "-nowarn");
     }
 
     @Parameters
