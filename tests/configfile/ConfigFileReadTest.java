22b6ce7167c759273768063c3853d3eb6af2c6e6
xingweitian <13183370+xingweitian@users.noreply.github.com>
2020-03-18 11:20:26 -0400

    Also apply googleJavaFormat to test files.

--- tests/configfile/ConfigFileReadTest.java
+++ tests/configfile/ConfigFileReadTest.java
@@ -1,32 +1,33 @@
-import org.checkerframework.checker.configfile.qual.ConfigFile;
-import org.checkerframework.checker.configfile.qual.ConfigFilePropertyValue;
-import org.checkerframework.common.value.qual.StringVal;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
+import org.checkerframework.checker.configfile.qual.ConfigFilePropertyValue;
 
 class ConfigFileReadTest {
 
     public static final String propFile = "tests/configfile/a.properties";
 
-    void a() throws IOException{
+    void a() throws IOException {
         Properties prop = new Properties();
         InputStream inputStream = getClass().getClassLoader().getResourceAsStream(propFile);
         prop.load(inputStream);
-        @ConfigFilePropertyValue("http://www.example.com") String url = prop.getProperty("URL");
+        @ConfigFilePropertyValue("http://www.example.com")
+        String url = prop.getProperty("URL");
     }
 
-    void b() throws IOException{
+    void b() throws IOException {
         Properties prop = new Properties();
         InputStream inputStream = getClass().getClassLoader().getResourceAsStream(propFile);
         prop.load(inputStream);
-        @ConfigFilePropertyValue("localhost") String host = prop.getProperty("HOST", "127.0.0.1");
+        @ConfigFilePropertyValue("localhost")
+        String host = prop.getProperty("HOST", "127.0.0.1");
     }
 
-    void c() throws IOException{
+    void c() throws IOException {
         Properties prop = new Properties();
         InputStream inputStream = getClass().getClassLoader().getResourceAsStream(propFile);
         prop.load(inputStream);
-        @ConfigFilePropertyValue("default value") String host = prop.getProperty("NOSUCHKEY", "default value");
+        @ConfigFilePropertyValue("default value")
+        String host = prop.getProperty("NOSUCHKEY", "default value");
     }
 }
