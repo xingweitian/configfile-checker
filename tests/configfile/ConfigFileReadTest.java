6f4a835d7b4ecca7ac3ac40809b6bd7cefaff5ff
xingweitian <13183370+xingweitian@users.noreply.github.com>
2020-03-18 02:59:54 -0400

    Tweaks.

--- tests/configfile/ConfigFileReadTest.java
+++ tests/configfile/ConfigFileReadTest.java
@@ -7,23 +7,25 @@ import java.util.Properties;
 
 class ConfigFileReadTest {
 
+    public static final String propFile = "tests/configfile/a.properties";
+
     void a() throws IOException{
         Properties prop = new Properties();
-        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("tests/configfile/a.properties");
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream(propFile);
         prop.load(inputStream);
         @ConfigFilePropertyValue("http://www.example.com") String url = prop.getProperty("URL");
     }
 
     void b() throws IOException{
         Properties prop = new Properties();
-        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("tests/configfile/a.properties");
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream(propFile);
         prop.load(inputStream);
         @ConfigFilePropertyValue("localhost") String host = prop.getProperty("HOST", "127.0.0.1");
     }
 
     void c() throws IOException{
         Properties prop = new Properties();
-        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("tests/configfile/a.properties");
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream(propFile);
         prop.load(inputStream);
         @ConfigFilePropertyValue("default value") String host = prop.getProperty("NOSUCHKEY", "default value");
     }
